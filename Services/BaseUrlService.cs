0f9f7d57e7783d53059551c4fa9bc54d946e2c08
springo <springoringo@gmail.com>
2021-12-24 22:57:54 -0500

    Common Endpoint

--- Services/BaseUrlService.cs
+++ Services/BaseUrlService.cs
@@ -1,5 +1,6 @@
-﻿using InBranchDashboard.Exceptions;
+﻿using KYCUploadAPICustomer.Exceptions;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using RestSharp;
 using System;
@@ -8,10 +9,15 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
-namespace InBranchDashboard.Services
+namespace KYCUploadAPICustomer.Services
 {
     public class BaseUrlService : IBaseUrlService
     {
+        private readonly ILogger<BaseUrlService> _logger;
+        public BaseUrlService(ILogger<BaseUrlService> logger)
+        {
+            _logger = logger;
+        }
 
         public class Status
         {
@@ -36,8 +42,8 @@ namespace InBranchDashboard.Services
 
         public async Task<string> BaseUrlLink()
         {
-            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-            var setingStringId = config.GetSection("ClientCallUrl").GetSection("SetingStringId").Value;
+
+            var setingStringId = "API_BASE_URL_DOCUMENT";
             var getUrl = await GetEndPointUrl(setingStringId);
             return getUrl.data.fieldValue;
 
@@ -46,8 +52,8 @@ namespace InBranchDashboard.Services
 
         public async Task<string> BaseUrlLinkForActiveDirectory()
         {
-            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-            var setingStringId = config.GetSection("ClientCallUrlForActiveDirectory").GetSection("SetingStringId").Value;
+
+            var setingStringId = "API_BASE_URL_AUTHENTICATION";
             var getUrl = await GetEndPointUrl(setingStringId);
             return getUrl.data.fieldValue;
 
@@ -57,10 +63,13 @@ namespace InBranchDashboard.Services
         private async Task<BaseUrlObject> GetEndPointUrl(string settingId)
         {
             var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-            var _commnserviceBaseUrl = config.GetSection("CommonService").GetSection("BaseUrl").Value;
+            var _commnserviceBaseUrl = config.GetSection("CommonEndpoint").GetSection("BaseUrl").Value;
 
+            _logger.LogCritical("This is the common endpoint:" + _commnserviceBaseUrl);
+            var fullUlr = _commnserviceBaseUrl + "api/Settings/FieldValue/" + settingId;
 
-            var client = new RestClient(_commnserviceBaseUrl + "common/api/Settings/FieldValue/" + settingId);
+            _logger.LogCritical("This is the full Ulr:" + fullUlr);
+            var client = new RestClient(fullUlr);
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
             IRestResponse response = await client.ExecuteAsync(request);
@@ -69,7 +78,7 @@ namespace InBranchDashboard.Services
             if (baseUrlObject == null)
             {
 
-                throw new HandleGeneralException(400, HttpStatusCode.BadRequest.ToString() + " : Unabale to Connect to Xtradot Service [#InBranchAUTH001-2-A] AuthenticateRestClient [GetXtradotAdUserDetails]");
+                throw new HandleGeneralException(503, HttpStatusCode.ServiceUnavailable.ToString() + " : Unabale to Connect to Xtradot Service [#InBranchAUTH001-2-A] AuthenticateRestClient [GetXtradotAdUserDetails]");
             }
             return baseUrlObject;
         }
